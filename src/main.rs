a43d5e080a61d2a264eecb4bb4ea7a67e355fc9d
Thom Bogers <t.bogers@ksyos.org>
2019-07-16 12:58:24 +0200

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -34,38 +34,15 @@ struct Metadata {
 fn main() {
     println!("Start rss builder!");
 
-    let file = fs::File::open("./data/casts.json")
-        .expect("file should open read only");
+    let casts = get_castdata();
+    let feed_items: Vec<rss::Item> = casts
+        .iter()
+        .map(get_feed_item)
+        .collect();
 
-    let json: serde_json::Value = serde_json::from_reader(file)
-        .expect("file should be proper JSON");
-    
-    let data = json.as_array()
-        .expect("file should contain JSON array");
+    let mut channel = get_channel();
+    channel.set_items(feed_items);
 
-    let mut itunes_extension = ITunesChannelExtension::default();
-    itunes_extension.set_author(get_metadata().author);
-    itunes_extension.set_image(get_data_link(&format!("{}.png", get_metadata().namespace), false));
-
-
-    let mut channel = rss::ChannelBuilder::default()
-        .title(get_metadata().title)
-        .description(get_metadata().description)
-        .link(get_data_link(&format!("{}.xml", get_metadata().namespace), false))
-        .itunes_ext(itunes_extension)
-        .build()
-        .unwrap();
-    
-    let mut items = channel.items().to_vec();
-
-    for index in 0..data.len() {
-        let item = data.get(index).unwrap().clone();
-        let cast: Cast = serde_json::from_value(item).unwrap();
-        let feed_item = get_feed_item(cast);
-        items.push(feed_item);
-    }
-
-    channel.set_items(items);
     let xml = channel.to_string();
     write_feed(&xml);
     println!("Result:\n{}", xml);
@@ -80,25 +57,23 @@ fn write_feed(feed: &str) {
 }
 
 fn get_data_link(filename: &str, namespaced: bool) -> String{
-    let api_key = get_metadata().api_key;
+    let metadata = get_metadata();
 
     let url = if namespaced {
-        format!("{}/{}", get_metadata().url, get_metadata().namespace)
+        format!("{}/{}", metadata.url, metadata.namespace)
     } else {
-        get_metadata().url
+        metadata.url
     };
 
-
-    let link = if api_key.len() > 0 {
-        format!("{}/{}?auth={}", url, filename, get_metadata().api_key)
+    let api_key = &metadata.api_key;
+    if api_key.len() > 0 {
+        format!("{}/{}?auth={}", url, filename, api_key)
     } else {
         format!("{}/{}", url, filename)
-    };
-
-    return link;
+    }
 }
 
-fn get_feed_item(cast: Cast) -> rss::Item {
+fn get_feed_item(cast: &Cast) -> rss::Item {
     println!("\tget feed item for cast: {:?}", cast);
 
     let file_meta = fs::metadata(format!("./data/{}", cast.filename))
@@ -120,19 +95,56 @@ fn get_feed_item(cast: Cast) -> rss::Item {
 
     rss::ItemBuilder::default()
         .link(get_data_link(&cast.filename, true))
-        .title(cast.episodename)
-        .author(cast.author)
-        .pub_date(cast.created_at)
+        .title(cast.episodename.clone())
+        .author(cast.author.clone())
+        .pub_date(cast.created_at.clone())
         .enclosure(enclosure)
         .categories(categories)
         .build()
         .unwrap()
 }
 
+fn get_channel() -> rss::Channel {
+    let metadata = get_metadata();
+
+    let mut itunes_extension = ITunesChannelExtension::default();
+    itunes_extension.set_author(metadata.author);
+    itunes_extension.set_image(get_data_link(&format!("{}.png", metadata.namespace), false));
+
+    rss::ChannelBuilder::default()
+        .title(metadata.title)
+        .description(metadata.description)
+        .link(get_data_link(&format!("{}.xml", metadata.namespace), false))
+        .itunes_ext(itunes_extension)
+        .build()
+        .unwrap()
+}
+
+fn get_castdata() -> Vec<Cast> {
+
+    let file = fs::File::open("./data/casts.json")
+        .expect("casts file should open read only");
+
+    let json: serde_json::Value = serde_json::from_reader(file)
+        .expect("casts file should be proper JSON");
+    
+    json.as_array()
+        .expect("casts file should contain JSON array")
+        .iter()
+        .map(|data| {
+            let cast: Cast = serde_json::from_value(data.clone())
+                .expect("cast item should be in correct format");
+            cast
+        })
+        .collect()
+}
+
 fn get_metadata() -> Metadata{
     let file = fs::File::open("./data/meta.json")
         .expect("file should open read only");
     
-    let meta: Metadata = serde_json::from_reader(file).expect("Could not parse metadata");
+    let meta: Metadata = serde_json::from_reader(file)
+        .expect("Could not parse metadata");
+        
     meta
 }
